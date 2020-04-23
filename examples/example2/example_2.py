Fixed governing PDE in peridynamics/kernels/opencl_euler_cromer.cl and /opencl_euler_cromer_optimised.cl, fixed non-zero initial damage bug caused by off by one error, tidied up main files

examples/example2/example_2.py
@@ -10,11 +10,9 @@ import numpy as np
 import pathlib
 from peridynamics import OpenCL
 from peridynamics.model import initial_crack_helper
-from peridynamics.integrators import DormandPrinceOptimised
+from peridynamics.integrators import EulerOpenCLOptimised
 from pstats import SortKey, Stats
-# TODO: add argument on command line that gives option to plot results or not,
-# as some systems won't have matplotlib installed.
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 import shutil
 import os
@@ -23,8 +21,6 @@ mesh_file_name = 'test.msh'
 mesh_file = pathlib.Path(__file__).parent.absolute() / mesh_file_name
 
 token_problems = ['test.msh', 'debug3D.msh', 'debug3D2.msh']
-verification_problems = ['1000beam2D.msh', '1000beam3D.msh', '1000beam3DT.msh']
-benchmark_problems = ['3300beam.msh']
 
 @initial_crack_helper
 def is_crack(x, y):
@@ -49,13 +45,7 @@ def is_crack(x, y):
 
 def is_tip(horizon, x):
     output = 0
-    if mesh_file_name in verification_problems:
-        if x[0] > 1.0 - 1./3 * horizon:
-            output = 1
-    elif mesh_file_name in benchmark_problems:
-        if x[0] > 3.3 - 0.5 * horizon:
-            output = 1
-    elif mesh_file_name in token_problems:
+    if mesh_file_name == 'test.msh':
         if x[0] > 1.0 - 1. * horizon:
             output = 1
     return output
@@ -64,66 +54,7 @@ def is_rebar(p):
     """ Function to determine whether the node coordinate is rebar
     """
     p = p[1:] # y and z coordinates for this node
-    if mesh_file_name == '3300beam.msh':
-        bar_centers = [
-            # Compressive bars 25mm of cover
-            np.array((0.031, 0.031)),
-            np.array((0.219, 0.031)),
-
-            # Tensile bars 25mm of cover
-            np.array((0.03825, 0.569)),
-            np.array((0.21175, 0.569))]
-
-        rad_c = 0.006
-        rad_t = 0.01325
-
-        radii = [
-            rad_c,
-            rad_c,
-            rad_t,
-            rad_t]
-
-        costs = [ np.sum(np.square(cent - p) - (np.square(rad))) for cent, rad in zip(bar_centers, radii) ]
-        if any( c <= 0 for c in costs ):
-            return True
-        else:
-            return False
-    elif mesh_file_name == '1000beam3DT.msh':
-        # Beam type 1 for flexural failure beam
-        # Beam type 2 for shear failure beam
-        beam_type = 2
-        if beam_type == 1:
-            bar_centers = [
-                    # Tensile bars 25mm of cover, WARNING: only gives 21.8mm inner spacing of bars
-                    np.array((0.0321, 0.185)),
-                    np.array((0.0679, 0.185))]
-            rad_t = 0.00705236
-            
-            radii = [
-                    rad_t,
-                    rad_t]
-            costs = [ np.sum(np.square(cent - p) - (np.square(rad))) for cent, rad in zip(bar_centers, radii) ]
-            if any( c <= 0 for c in costs ):
-                return True
-            else:
-                return False
-        elif beam_type ==2:
-            bar_centers = [
-                    # Tensile bars 25mm of cover, WARNING: only gives 7.6mm inner spacing of bars
-                    np.array((0.0356, 0.185)),
-                    np.array((0.0644, 0.185))]
-            rad_t = 0.0105786
-            
-            radii = [
-                    rad_t,
-                    rad_t]
-            costs = [ np.sum(np.square(cent - p) - (np.square(rad))) for cent, rad in zip(bar_centers, radii) ]
-            if any( c <= 0 for c in costs ):
-                return True
-            else:
-                return False
-    else:
-        return False
+    return False
 
 def bond_type(x, y):
     """ 
@@ -134,8 +65,8 @@ def bond_type(x, y):
         'plain = 0' will return a concrete beam with some rebar as specified
         in "is_rebar()"
     """
-    plain = 0
-    output = 0 # default to concrete
+    plain = 1
+    output = 'concrete' # default to concrete
     bool1 = is_rebar(x)
     bool2 = is_rebar(y)
     if plain == 1:
@@ -156,7 +87,7 @@ def is_boundary(horizon, x):
     1 is displacement loaded IN +ve direction
     0 is clamped boundary
     """
-    if mesh_file_name in token_problems:
+    if mesh_file_name == 'test.msh':
         # Does not live on a boundary
         bnd = 2
         # Does live on boundary
@@ -164,22 +95,6 @@ def is_boundary(horizon, x):
             bnd = -1
         elif x[0] > 1.0 - 1.5 * horizon:
             bnd = 1
-    elif mesh_file_name in verification_problems:
-        # Does not live on a boundary
-        bnd = 2
-        # Does live on boundary
-        if x[0] < 1.5* horizon:
-            bnd = 0
-        if x[0] > 1.0 - 1.* horizon:
-            if x[2] > 0.2 - 1.* horizon:
-                bnd = 1
-    elif mesh_file_name == '3300beam.msh':
-        bnd = 2
-        if x[0] < 1.5 * horizon:
-            bnd = 0
-        if x[0] > 3.3 - 0.3* horizon:
-            if x[2] > 0.6 - 0.3*horizon:
-                bnd = 1
     return bnd
 
 def is_forces_boundary(horizon, x):
@@ -189,25 +104,9 @@ def is_forces_boundary(horizon, x):
     -1 is force loaded IN -ve direction
     1 is force loaded IN +ve direction
     """
-    if mesh_file_name in token_problems:
+    if mesh_file_name == 'test.msh':
         bnd = 2
         if x[0] > 1.0 - 1.5 * horizon:
-            bnd = 2
-    elif mesh_file_name == '1000beam2D.msh':
-        bnd = 2
-        if x[1] > 0.2 - 1./3 * horizon:
-            bnd = 2
-    elif mesh_file_name == '1000beam3DT.msh':
-        bnd = 2
-        if x[2] > 0.2 - 1. * horizon:
-            bnd = -1
-    elif mesh_file_name in verification_problems:
-        bnd = 2
-        if x[0] > 1.0 - 1. * horizon:
-            bnd = -1
-    elif mesh_file_name == '3300beam.msh':
-        bnd = 2
-        if x[2] > 0.6 - 1. * horizon:
             bnd = 2
     return bnd
 
@@ -217,8 +116,7 @@ def boundary_function(model):
     also define the 'tip' (for plotting displacements)
     """
     load_rate = 1e-5
-    #theta = 18.75
-    # initiate
+    #initiate containers
     model.bc_types = np.zeros((model.nnodes, model.degrees_freedom), dtype=np.intc)
     model.bc_values = np.zeros((model.nnodes, model.degrees_freedom), dtype=np.float64)
     model.tip_types = np.zeros(model.nnodes, dtype=np.intc)
@@ -231,12 +129,10 @@ def boundary_function(model):
         model.bc_types[i, 1] = np.intc((bnd))
         model.bc_types[i, 2] = np.intc((bnd))
         model.bc_values[i, 0] = np.float64(bnd * 0.5 * load_rate)
-        #model.bc_values[i, 0] = np.float64(bnd * -0.5/theta * load_rate)
 
         # Define tip here
         tip = is_tip(model.horizon, model.coords[i][:])
         model.tip_types[i] = np.intc(tip)
-    print(np.max(model.tip_types), 'max_tip_types')
 
 def boundary_forces_function(model):
     """ 
@@ -281,59 +177,44 @@ def main():
         profile.enable()
 
     st = time.time()
-
-    volume_total = 1.0
-    density_concrete = 1
-    self_weight = 1.*density_concrete * volume_total * 9.81
-# =============================================================================
-#     # Sength scale for covariance matrix
-#     l = 1e-2
-#     # Vertical scale of the covariance matrix
-#     nu = 9e-4
-#     model = OpenCLProbabilistic(mesh_file_name, volume_total, nu, l, bond_type=bond_type, initial_crack=is_crack)
-# =============================================================================
-    model = OpenCL(mesh_file_name, volume_total, bond_type=bond_type, initial_crack=is_crack)
-    #dx = np.power(1.*volume_total/model.nnodes,1./(model.dimensions))
+    
+    horizon = 0.1
     # Set simulation parameters
-    # not a transfinite mesh
-    model.transfinite = 0
-    # do precise stiffness correction factors
-    model.precise_stiffness_correction = 1
-    # Only one material in this example, that is 'concrete'
-    model.density = density_concrete
-    #self.horizon = dx * np.pi 
-    model.horizon = 0.1
-    model.family_volume = np.pi * np.power(model.horizon, 2)
-    model.damping = 1 # damping term
-    # Peridynamic bond stiffness, c
-    model.bond_stiffness_concrete = (
-            np.double((18.00 * 0.05) /
-            (np.pi * np.power(model.horizon, 4)))
-            )
-    model.critical_strain_concrete = 0.005
-    model.crackLength = np.double(0.3)
+    model = OpenCL(mesh_file_name, 
+               density = 1.0, 
+               horizon = horizon,
+               damping = 1.0,
+               bond_stiffness_concrete = (
+                       np.double((18.00 * 0.05) /
+                                 (np.pi * np.power(horizon, 4)))
+                       ),
+               critical_strain_concrete = 0.005,
+               crack_length = 0.3,
+               volume_total=1.0,
+               bond_type=bond_type,
+               network_file_name = 'Network.vtk',
+               initial_crack=[],
+               dimensions=2,
+               transfinite=0,
+               precise_stiffness_correction=1)
     model.dt = np.double(0.5e-3)
-    model.max_reaction = 1.* self_weight # in newtons, about 85 * self weight
-    model.load_scale_rate = 1/1000
     # Set force and displacement boundary conditions
     boundary_function(model)
     boundary_forces_function(model)
     # delete output directory contents, this is probably unsafe?
     shutil.rmtree('./output', ignore_errors=False)
     os.mkdir('./output')
-    integrator = DormandPrinceOptimised(model)
+    integrator = EulerOpenCLOptimised(model)
     damage_sum_data, tip_displacement_data, tip_shear_force_data = model.simulate(model, sample=1, steps=350, integrator=integrator, write=350, toolbar=0)
     print('damage_sum_data', damage_sum_data)
-# =============================================================================
-#     plt.figure(1)
-#     plt.title('damage over time')
-#     plt.plot(damage_sum_data)
-#     plt.figure(2)
-#     plt.title('tip displacement over time')
-#     plt.plot(tip_displacement_data)
-#     plt.show()
-# =============================================================================
     print('TOTAL TIME REQUIRED {}'.format(time.time() - st))
+    plt.figure(1)
+    plt.title('damage over time')
+    plt.plot(damage_sum_data)
+    plt.figure(2)
+    plt.title('tip displacement over time')
+    plt.plot(tip_displacement_data)
+    plt.show()
     if args.profile:
         profile.disable()
         s = StringIO()

examples/example4/example4.py
@@ -10,11 +10,11 @@ import numpy as np
 import pathlib
 from peridynamics import OpenCL
 from peridynamics.model import initial_crack_helper
-from peridynamics.integrators import EulerOpenCL
+from peridynamics.integrators import EulerCromer
 from pstats import SortKey, Stats
 # TODO: add argument on command line that gives option to plot results or not,
 # as some systems won't have matplotlib installed.
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 import shutil
 import os
@@ -22,9 +22,6 @@ import os
 mesh_file_name = '3300beam.msh'
 mesh_file = pathlib.Path(__file__).parent.absolute() / mesh_file_name
 
-token_problems = ['test.msh', 'debug3D.msh', 'debug3D2.msh']
-verification_problems = ['1000beam2D.msh', '1000beam3D.msh', '1000beam3DT.msh']
-benchmark_problems = ['3300beam.msh']
 
 @initial_crack_helper
 def is_crack(x, y):
@@ -33,14 +30,8 @@ def is_crack(x, y):
 
 def is_tip(horizon, x):
     output = 0
-    if mesh_file_name in verification_problems:
-        if x[0] > 1.0 - 1./3 * horizon:
-            output = 1
-    elif mesh_file_name in benchmark_problems:
-        if x[0] > 3.3 - 0.5 * horizon:
-            output = 1
-    elif mesh_file_name in token_problems:
-        if x[0] > 1.0 - 1. * horizon:
+    if mesh_file_name == '3300beam.msh':
+        if x[0] > 3.3 - 0.2 * horizon:
             output = 1
     return output
 
@@ -72,40 +63,6 @@ def is_rebar(p):
             return True
         else:
             return False
-    elif mesh_file_name == '1000beam3DT.msh':
-        # Beam type 1 for flexural failure beam
-        # Beam type 2 for shear failure beam
-        beam_type = 2
-        if beam_type == 1:
-            bar_centers = [
-                    # Tensile bars 25mm of cover, WARNING: only gives 21.8mm inner spacing of bars
-                    np.array((0.0321, 0.185)),
-                    np.array((0.0679, 0.185))]
-            rad_t = 0.00705236
-            
-            radii = [
-                    rad_t,
-                    rad_t]
-            costs = [ np.sum(np.square(cent - p) - (np.square(rad))) for cent, rad in zip(bar_centers, radii) ]
-            if any( c <= 0 for c in costs ):
-                return True
-            else:
-                return False
-        elif beam_type ==2:
-            bar_centers = [
-                    # Tensile bars 25mm of cover, WARNING: only gives 7.6mm inner spacing of bars
-                    np.array((0.0356, 0.185)),
-                    np.array((0.0644, 0.185))]
-            rad_t = 0.0105786
-            
-            radii = [
-                    rad_t,
-                    rad_t]
-            costs = [ np.sum(np.square(cent - p) - (np.square(rad))) for cent, rad in zip(bar_centers, radii) ]
-            if any( c <= 0 for c in costs ):
-                return True
-            else:
-                return False
     else:
         return False
 
@@ -147,7 +104,7 @@ def is_boundary(horizon, x):
             bnd[1] = 0
             bnd[2] = 0
         if x[0] > 3.3 - 0.2* horizon:
-            bnd[2] = 1
+            bnd[2] = 2
     return bnd
 
 def is_forces_boundary(horizon, x):
@@ -157,26 +114,10 @@ def is_forces_boundary(horizon, x):
     -1 is force loaded IN -ve direction
     1 is force loaded IN +ve direction
     """
-    if mesh_file_name in token_problems:
+    if mesh_file_name == '3300beam.msh':
         bnd = 2
-        if x[0] > 1.0 - 1.5 * horizon:
-            bnd = 2
-    elif mesh_file_name == '1000beam2D.msh':
-        bnd = 2
-        if x[1] > 0.2 - 1./3 * horizon:
-            bnd = 2
-    elif mesh_file_name == '1000beam3DT.msh':
-        bnd = 2
-        if x[2] > 0.2 - 1. * horizon:
+        if x[0] > 3.3 - 0.2 * horizon:
             bnd = -1
-    elif mesh_file_name in verification_problems:
-        bnd = 2
-        if x[0] > 1.0 - 1. * horizon:
-            bnd = -1
-    elif mesh_file_name == '3300beam.msh':
-        bnd = 2
-        if x[2] > 0.6 - 1. * horizon:
-            bnd = 2
     return bnd
 
 def boundary_function(model):
@@ -226,10 +167,11 @@ def boundary_forces_function(model):
         model.force_bc_types[i, 2] = np.intc((bnd))
 
     model.num_force_bc_nodes = num_force_bc_nodes
+    print(num_force_bc_nodes)
 
     # Calculate initial forces
     model.force_bc_values = np.zeros((model.nnodes, model.degrees_freedom), dtype=np.float64)
-    load_scale = 0.0
+    load_scale = 1.0
     for i in range(0, model.nnodes):
         bnd = is_forces_boundary(model.horizon, model.coords[i][:])
         if bnd == 1:
@@ -257,6 +199,31 @@ def main():
     youngs_modulus_concrete = 1.*22e9
     youngs_modulus_steel = 1.*210e9
     tensile_strength_concrete = 2.6e6
+    poisson_ratio = 0.25
+    # Set simulation parameters
+    # Two materials in this example, that is 'concrete' and 'steel'
+    dx = np.power(1.*volume_total/4625,1./(3))
+    horizon = dx * np.pi 
+    family_volume =(4./3)*np.pi*np.power(horizon, 3)
+    damping = 2.0e6 # damping term
+    # Peridynamic bond stiffness, c
+    bulk_modulus_concrete = youngs_modulus_concrete/ (3* (1 - 2*poisson_ratio))
+    bulk_modulus_steel = youngs_modulus_steel / (3* (1- 2*poisson_ratio))
+    bond_stiffness_concrete = (
+    np.double((18.00 * bulk_modulus_concrete) /
+    (np.pi * np.power(horizon, 4)))
+    )
+    bond_stiffness_steel = (
+    np.double((18.00 * bulk_modulus_steel) /
+    (np.pi * np.power(horizon, 4)))
+    )
+    critical_strain_concrete = (
+    np.double(tensile_strength_concrete /
+    youngs_modulus_concrete)
+    )
+    #model.critical_strain_concrete = np.double(0.000533) # check this value
+    critical_strain_steel = np.double(0.01)
+    crack_length = np.double(0.0)
 # =============================================================================
 #     # Sength scale for covariance matrix
 #     l = 1e-2
@@ -264,68 +231,48 @@ def main():
 #     nu = 9e-20
 #     model = OpenCLProbabilistic(mesh_file_name, volume_total, nu, l, bond_type=bond_type, initial_crack=is_crack)
 # =============================================================================
-    model = OpenCL(mesh_file_name, volume_total, bond_type=bond_type, initial_crack=is_crack, dimensions=3)
-    # Set simulation parameters
-    # not a transfinite mesh
-    model.transfinite = 0
-    # do precise stiffness correction factors
-    model.precise_stiffness_correction = 1
-    # Only one material in this example, that is 'concrete'
-    model.density = density_concrete
-    dx = np.power(1.*volume_total/model.nnodes,1./(model.dimensions))
-    model.horizon = dx * np.pi 
-    model.family_volume =(4./3)*np.pi*np.power(model.horizon, 3)
-    model.damping = 2.0e6 # damping term
-    # Peridynamic bond stiffness, c
-    bulk_modulus_concrete = youngs_modulus_concrete/ (3* (1 - 2*model.poisson_ratio))
-    bulk_modulus_steel = youngs_modulus_steel / (3* (1- 2*model.poisson_ratio))
-    model.bond_stiffness_concrete = (
-    np.double((18.00 * bulk_modulus_concrete) /
-    (np.pi * np.power(model.horizon, 4)))
-    )
-    model.bond_stiffness_steel = (
-    np.double((18.00 * bulk_modulus_steel) /
-    (np.pi * np.power(model.horizon, 4)))
-    )
-    model.critical_strain_concrete = (
-    np.double(tensile_strength_concrete /
-    youngs_modulus_concrete)
-    )
-    #model.critical_strain_concrete = np.double(0.000533) # check this value
-    model.critical_strain_steel = np.double(0.01)
-    model.crackLength = np.double(0.3)
-    #saf_fac = 0.2 # Typical values 0.70 to 0.95 (Sandia PeridynamicSoftwareRoadmap)
-    #model.dt = (
-    # 0.8 * np.power( 2.0 * density_concrete * dx / 
-    # (np.pi * np.power(model.horizon, 2.0) * dx * model.bond_stiffness_concrete), 0.5)
-    # * saf_fac
-    # )
-    #model.dt = 1.25e-8
-    model.dt = 5.7e-14
-    model.max_reaction = 1.* self_weight # in newtons, about 85 times self weight
-    model.load_scale_rate = 1
+    model = OpenCL(mesh_file_name, density = density_concrete, horizon = horizon, family_volume = family_volume, 
+                 damping = damping, bond_stiffness_concrete = bond_stiffness_concrete, bond_stiffness_steel = bond_stiffness_steel, 
+                 critical_strain_concrete = critical_strain_concrete, critical_strain_steel = critical_strain_steel, crack_length = crack_length,
+                 volume_total=volume_total, bond_type=bond_type, network_file_name = 'Network.vtk', initial_crack=[], dimensions=3,
+                 transfinite=1, precise_stiffness_correction=0)
+    saf_fac = 0.7 # Typical values 0.70 to 0.95 (Sandia PeridynamicSoftwareRoadmap)
+    model.dt = (
+     0.8 * np.power( 2.0 * density_concrete * dx / 
+     (np.pi * np.power(model.horizon, 2.0) * dx * model.bond_stiffness_concrete), 0.5)
+     * saf_fac
+     )
+    #model.dt = 5.0e-7
+    #model.dt = 5.7e-14
+    #model.max_reaction = 1.* self_weight # in newtons, about 85 times self weight
+    model.max_reaction = 100000000 # in newtons, about 85 times self weight
+    model.load_scale_rate = 1/1000
 
     # Set force and displacement boundary conditions
     boundary_function(model)
     boundary_forces_function(model)
 
-    integrator = EulerOpenCL(model)
+    integrator = EulerCromer(model)
 
     # delete output directory contents, this is probably unsafe?
     shutil.rmtree('./output', ignore_errors=False)
     os.mkdir('./output')
 
-    damage_data, damage_sum_data, tip_displacement_data = model.simulate(model, sample=1, steps=2000, integrator=integrator, write=2000, toolbar=0)
-# =============================================================================
-#     plt.figure(1)
-#     plt.title('damage over time')
-#     plt.plot(damage_data)
-#     plt.figure(2)
-#     plt.title('tip displacement over time')
-#     plt.plot(tip_displacement_data)
-#     plt.show()
-# =============================================================================
+    damage_sum_data, tip_displacement_data, tip_shear_force_data = model.simulate(model, sample=1, steps=15000, integrator=integrator, write=1000, toolbar=0)
+    plt.figure(1)
+    plt.title('damage over time')
+    plt.plot(damage_sum_data)
+    plt.figure(2)
+    plt.title('tip displacement over time')
+    plt.plot(tip_displacement_data)
+    plt.show()
+    plt.figure(3)
+    plt.title('shear force over time')
+    plt.plot(tip_shear_force_data)
+    plt.show()
     print(damage_sum_data)
+    print(tip_displacement_data)
+    print(tip_shear_force_data)
     print('TOTAL TIME REQUIRED {}'.format(time.time() - st))
     if args.profile:
         profile.disable()

examples/probabilistic/example_MCMC.py
@@ -13,12 +13,10 @@ from peridynamics.model import initial_crack_helper
 from peridynamics.integrators import EulerStochastic
 from pstats import SortKey, Stats
 import scipy.stats as sp
-#import matplotlib.pyplot as plt
 import shutil
 import os
 import mcmc
 import csv
-
 mesh_file_name = 'test.msh'
 mesh_file = pathlib.Path(__file__).parent.absolute() / mesh_file_name
 
@@ -213,8 +211,8 @@ def main():
     # Sength scale for covariance matrix
     l = 1e-2
     # Vertical scale of the covariance matrix
-    nu = 9e-4
-    model = OpenCLProbabilistic(mesh_file_name, volume_total, nu, l, bond_type=bond_type, initial_crack=is_crack)
+    sigma = 9e-4
+    model = OpenCLProbabilistic(mesh_file_name, volume_total, sigma, l, bond_type=bond_type, initial_crack=is_crack)
     #dx = np.power(1.*volume_total/model.nnodes,1./(model.dimensions))
     # Set simulation parameters
     # not a transfinite mesh
@@ -247,9 +245,9 @@ def main():
     # read the data
     damage_data = read_data(model)
     samples = 10
-    realisations = 3
+    realisations = 1
     
-    # Define start point of the Metropolis Hastings sampler w[1] is lambda, w[0] is sigma
+    # Define start point of the Metropolis Hastings sampler w[1] is l, w[0] is sigma
     w_prev = [-6.51, -5.05]
     
     # Define proposal density of the MCMC sampler
@@ -275,6 +273,7 @@ def main():
     
     for sample in range(samples):
         total_samples += 1
+        print('Sample {}/{} Complete'.format(total_samples, samples))
         # Get proposal parameters
         w = sp.multivariate_normal.rvs(w_prev, w_cov, 1)
         # update (l, sigma)
@@ -302,9 +301,13 @@ def main():
                 # accept the sample
                 data[0].append(w[0])
                 data[1].append(w[1])
+                print('accepted sigma: ', w[0], 'accepted l: ', w[1])
                 w_prev = w
                 prior_prev = prior
                 likelihood_prev = likelihood
+                with open(pathlib.Path(__file__).parent.absolute() / "mcmc_current.csv", 'a', newline='') as myfile:
+                    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+                    wr.writerow([w[0],w[1]])
             else:
                 None
     
@@ -314,26 +317,18 @@ def main():
     data[0] = data[0][burn:]
     data[1] = data[1][burn:]
     
-    print(data)
-# =============================================================================
-#     NO_BINS = 10
-#     xstart = -1.5
-#     xfinish = 1.5
-#     ystart = -6.0
-#     yfinish = -3.0
-#     xedges = np.linspace(xstart, xfinish, NO_BINS)
-#     yedges = np.linspace(ystart, yfinish, NO_BINS)
-#     H, xedges, yedges = np.histogram2d(data[0], data[1], bins=(xedges, yedges))
-# =============================================================================
-    #print('The percentage of accepted samples was {}%'.format(len(data[0])*100/(total_samples)))
-    
-# =============================================================================
-#     plt.figure()
-#     plt.hist2d(data[0], data[1], bins=100)
-#     plt.xlabel('$\sigma$')
-#     plt.ylabel('$\zeta$')
-#     plt.show()
-# =============================================================================
+    print(len(data[0]), 'LENGTH')
+    mean = np.divide(np.sum(data, axis = 1), len(data[0]))
+    print(mean, mean.shape, 'MEAN AND SHAPE')
+        
+    with open(pathlib.Path(__file__).parent.absolute() / "mean.csv", "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        for val in mean:
+            writer.writerow([val])
+    if total_samples == 0:
+        pass
+    else:
+        print('The percentage of accepted samples was {}%'.format(len(data[0])*100/(total_samples)))
     
     # Write data to a file
     with open(pathlib.Path(__file__).parent.absolute() / "mcmc.csv", 'w', newline='') as myfile:

examples/probabilistic/mcmc.py
@@ -7,6 +7,7 @@ Created on Thu Mar  5 08:16:27 2020
 import numpy as np
 import scipy.stats as sp
 from scipy.special import gamma
+from matplotlib import pyplot as plt
 
 def gamma_prior_pdf(x, alpha =1., beta=100.0):
     """ pdf of zeta rv with values of alpha and beta
@@ -25,10 +26,31 @@ def get_fast_likelihood(damage_data, model_sample):
     print(nll)
     likelihood = np.exp(-1.*nll)
     print(likelihood)
+    return nll
+
+def get_log_likelihood(damage_data, model_sample):
+    # Assume idependent, identically distributed with a variance of 1
+    error = np.subtract(model_sample, damage_data)
+    l2 = np.dot(error, error)
+    nll = 1./2 * l2
+    print(nll)
+    likelihood = np.exp(-1.*nll)
+    print(likelihood)
+    return nll
+
+def get_likelihood(damage_data, model_sample):
+    # Assume idependent, identically distributed with a variance of 1
+    error = np.subtract(model_sample, damage_data)
+    l2 = np.dot(error, error)
+    nll = 1./2 * l2
+    likelihood = np.exp(-1.*nll)
+    print(likelihood)
     return likelihood
 
-def get_KL_divergence(damage_data, model_sample):
-    # Taking the damage_data as ground truth
+# =============================================================================
+# def get_KL_divergence(damage_data, model_sample):
+#     # Taking the damage_data as ground truth
+# =============================================================================
 
 def beta_likelihood(damage_data, model_sample):
     mode = model_sample
@@ -40,3 +62,12 @@ def beta_likelihood(damage_data, model_sample):
         log_likelihood += np.log(likelihood)
     total_likelihood = np.exp(log_likelihood)
     return total_likelihood
+
+
+# =============================================================================
+# x = np.linspace(-10.0, -2.0, 50)
+# 
+# y = gamma_prior_pdf(x)
+# 
+# plt.plot(x,y)
+# =============================================================================

peridynamics/integrators.py
@@ -2800,14 +2800,14 @@ class EulerStochastic(Integrator):
         """
     def reset(self, model, steps):
         # Displacements
-        self.h_un = np.empty((model.nnodes, model.degrees_freedom), dtype=np.float64)
+        self.h_un = np.zeros((model.nnodes, model.degrees_freedom), dtype=np.float64)
 
         # Forces
-        self.h_udn = np.empty((model.nnodes, model.degrees_freedom), dtype=np.float64)
-        self.h_udn1 = np.empty((model.nnodes, model.degrees_freedom), dtype=np.float64)
+        self.h_udn = np.zeros((model.nnodes, model.degrees_freedom), dtype=np.float64)
+        #self.h_udn1 = np.zeros((model.nnodes, model.degrees_freedom), dtype=np.float64)
 
         # Damage vector
-        self.h_damage = np.empty(model.nnodes).astype(np.float64)
+        self.h_damage = np.zeros(model.nnodes).astype(np.float64)
 
         # Sample random noise vector
         self.h_pn = noise(model.C, model.K, model.nnodes, steps)
@@ -2829,11 +2829,11 @@ class EulerStochastic(Integrator):
         self.d_horizons = cl.Buffer(
                 self.context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR,
                 hostbuf=self.h_horizons)
-        self.d_un = cl.Buffer(self.context, cl.mem_flags.READ_WRITE, self.h_un.nbytes)
-        self.d_udn = cl.Buffer(self.context, cl.mem_flags.READ_WRITE, self.h_udn1.nbytes)
-        self.d_udn1 = cl.Buffer(self.context, cl.mem_flags.READ_WRITE, self.h_udn1.nbytes)
+        self.d_un = cl.Buffer(self.context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=self.h_un)
+        self.d_udn = cl.Buffer(self.context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=self.h_udn)
+        #self.d_udn1 = cl.Buffer(self.context, cl.mem_flags.READ_WRITE, self.h_udn1.nbytes)
         # Write only
-        self.d_damage = cl.Buffer(self.context, cl.mem_flags.WRITE_ONLY, self.h_damage.nbytes)
+        self.d_damage = cl.Buffer(self.context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=self.h_damage)
         # Initialize kernel parameters
     def runtime(self, model, step):
         # Time marching Part 1
@@ -2855,10 +2855,10 @@ class EulerStochastic(Integrator):
                                   (model.max_horizon_length,), self.d_horizons,
                                            self.d_horizons_lengths, self.d_damage, self.local_mem)
         cl.enqueue_copy(self.queue, self.h_damage, self.d_damage)
-        #cl.enqueue_copy(self.queue, self.h_un, self.d_un)
-        #vtk.write("output/U_"+"sample" + str(sample) +"realisation" +str(realisation) +"t" +str(t) + ".vtk", "Solution time step = "+str(t),
-                  #model.coords, self.h_damage, self.h_un)
-        #vtk.writeDamage("output/damage_" + str(t)+ "sample" + str(sample) + ".vtk", "Title", self.h_damage)
+        cl.enqueue_copy(self.queue, self.h_un, self.d_un)
+        vtk.write("output/U_"+"sample" + str(sample) +"realisation" +str(realisation) + ".vtk", "Solution time step = "+str(t),
+                  model.coords, self.h_damage, self.h_un)
+        vtk.writeDamage("output/damage_" + "sample" + str(sample)+ "realisation" +str(realisation) + ".vtk", "Title", self.h_damage)
         return self.h_damage
 
 # =============================================================================

peridynamics/model.py
@@ -9,7 +9,6 @@ from scipy.spatial.distance import cdist
 import time
 import sys
 from .post_processing import vtk
-import peridynamics.grid as fem
 
 _MeshElements = namedtuple("MeshElements", ["connectivity", "boundary"])
 _mesh_elements_2d = _MeshElements(connectivity="triangle",
@@ -180,10 +179,10 @@ class Model:
             self.nnodes = self.coords.shape[0]
 
             # Get connectivity, mesh triangle cells
-            self.mesh_connectivity = mesh.cells_dict[self.mesh_elements.connectivity]
+            self.mesh_connectivity = mesh.cells[self.mesh_elements.connectivity]
 
             # Get boundary connectivity, mesh lines
-            self.mesh_boundary = mesh.cells_dict[self.mesh_elements.boundary]
+            self.mesh_boundary = mesh.cells[self.mesh_elements.boundary]
 
             # Get number elements on boundary?
             self.nelem_bnd = self.mesh_boundary.shape[0]
@@ -556,7 +555,7 @@ class OpenCL(Model):
     This class allows users to define a peridynamics system from parameters and
     a set of initial conditions (coordinates and connectivity).
     """
-    def __init__(self, mesh_file_name, density = None, horizon = None, family_volume = None, 
+    def __init__(self, mesh_file_name, density = None, horizon = None, 
                  damping = None, bond_stiffness_concrete = None, bond_stiffness_steel = None, 
                  critical_strain_concrete = None, critical_strain_steel = None, crack_length = None,
                  volume_total=None, bond_type=None, network_file_name = 'Network.vtk', initial_crack=[], dimensions=2,
@@ -564,11 +563,14 @@ class OpenCL(Model):
         """
         Construct a :class:`OpenCL` object, which inherits Model class.
         
-        :arg str mesh_file: Path of the mesh file defining the systems nodes
+        :arg str mesh_file_name: Path of the mesh file defining the systems nodes
             and connectivity.
+        :arg float density: Density of the bulk material in kg/m^3
         :arg float horizon: The horizon radius. Nodes within `horizon` of
             another interact with that node and are said to be within its
             neighbourhood.
+        :arg float family_volume: The spherical volume defined by the horizon
+        radius.
         :arg float critical_strain: The critical strain of the model. Bonds
             which exceed this strain are permanently broken.
         :arg float elastic_modulus: The appropriate elastic modulus of the
@@ -593,8 +595,10 @@ class OpenCL(Model):
 
         if dimensions == 2:
             self.mesh_elements = _mesh_elements_2d
+            self.family_volume = np.pi*np.power(horizon, 2)
         elif dimensions == 3:
             self.mesh_elements = _mesh_elements_3d
+            self.family_volume = (4./3)*np.pi*np.power(horizon, 3)
         else:
             raise DimensionalityError(dimensions)
 
@@ -618,13 +622,13 @@ class OpenCL(Model):
         # These are the parameters that the user needs to define
         self.density = density
         self.horizon = horizon
-        self.family_volume = family_volume
         self.damping = damping
         self.bond_stiffness_concrete = bond_stiffness_concrete
         self.bond_stiffness_steel = bond_stiffness_steel
         self.critical_strain_concrete = critical_strain_concrete
         self.critical_strain_steel = critical_strain_steel
         self.crack_length = crack_length
+        self.volume_total = volume_total
         self.dt = None
         self.max_reaction = None
         self.load_scale_rate = None
@@ -898,7 +902,7 @@ class OpenCL(Model):
                             tmp2.append(self.bond_stiffness_steel)
                             tmp3.append(self.critical_strain_steel)
                         elif material_flag == 'interface':
-                            tmp2.append(self.bond_stiffness_concrete) # choose the weakest stiffness of the two bond types
+                            tmp2.append(self.bond_stiffness_concrete * 3.0) # factor of 3 is used for interface bonds in the literature
                             tmp3.append(self.critical_strain_concrete * 3.0) # 3.0 is used for interface bonds in the literature
                         elif material_flag == 'concrete':
                             tmp2.append(self.bond_stiffness_concrete)
@@ -934,20 +938,22 @@ class OpenCL(Model):
             # Calculate stiffening factor nore accurately using actual nodal volumes
             for i in range(0, self.nnodes):
                 family_list = family[i]
-                nodei_family_volume = self.family_v[i] # Possible to calculate more exactly, we have the volumes for free
+                nodei_family_volume = self.family_v[i]
                 for j in range(len(family_list)):
                     nodej_family_volume = self.family_v[j]
                     stiffening_factor = 2.* self.family_volume /  (nodej_family_volume + nodei_family_volume)
                     print('Stiffening factor {}'.format(stiffening_factor))
                     bond_stiffness_family[i][j] *= stiffening_factor
         elif self.precise_stiffness_correction == 0:
+            # TODO: check this code, it was 23:52pm
+            average_node_volume = self.volume_total/self.nnodes
             # Calculate stiffening factor - surface corrections for 3D problem, for this we need family matrix
             for i in range(0, self.nnodes):
                 nnodes_i_family = len(family[i])
-                nodei_family_volume = nnodes_i_family * self.average_node_volume # Possible to calculate more exactly, we have the volumes for free
+                nodei_family_volume = nnodes_i_family * average_node_volume # Possible to calculate more exactly, we have the volumes for free
                 for j in range(len(family[i])):
                     nnodes_j_family = len(family[j])
-                    nodej_family_volume = nnodes_j_family* self.average_node_volume # Possible to calculate more exactly, we have the volumes for free
+                    nodej_family_volume = nnodes_j_family* average_node_volume # Possible to calculate more exactly, we have the volumes for free
                     
                     stiffening_factor = 2.* self.family_volume /  (nodej_family_volume + nodei_family_volume)
                     
@@ -1107,9 +1113,12 @@ class OpenCL(Model):
                         st = time.time()
 
             # Increase load in linear increments
-            load_scale = min(1.0, model.load_scale_rate * step)
-            if load_scale != 1.0:
-                integrator.incrementLoad(model, load_scale)
+            if model.load_scale_rate is None:
+                pass
+            else:
+                load_scale = min(1.0, model.load_scale_rate * step)
+                if load_scale != 1.0:
+                    integrator.incrementLoad(model, load_scale)
             # Loading bar update
             if step%(steps/toolbar_width)<1 & toolbar:
                 sys.stdout.write("\u2588")
@@ -1125,7 +1134,7 @@ class OpenCLProbabilistic(OpenCL):
     This class allows users to define a peridynamics system from parameters and
     a set of initial conditions (coordinates and connectivity).
     """
-    def __init__(self, mesh_file_name, volume_total, nu, l, bond_type, network_file_name = 'Network.vtk', initial_crack=[], dimensions=2):
+    def __init__(self, mesh_file_name, volume_total, sigma, l, bond_type, network_file_name = 'Network.vtk', initial_crack=[], dimensions=2):
         """
         Construct a :class:`OpenCL` object, which inherits Model class.
         
@@ -1199,7 +1208,7 @@ class OpenCLProbabilistic(OpenCL):
 
         self._set_volume(volume_total)
         # Set covariance matrix
-        self._set_H(l, nu)
+        self._set_H(l, sigma)
         # If the network has already been written to file, then read, if not, setNetwork
         self._read_network(network_file_name)
 # =============================================================================
@@ -1225,7 +1234,7 @@ class OpenCLProbabilistic(OpenCL):
         if self.v == True:
             print("sum total volume", self.sum_total_volume)
             print("user input volume total", volume_total)
-    def _set_H(self, l, nu, epsilon=1e-5):
+    def _set_H(self, l, sigma, epsilon=1e-5):
         """
         Constructs the failure strains matrix and H matrix, which is a sparse
         matrix containing distances.
@@ -1259,21 +1268,21 @@ class OpenCLProbabilistic(OpenCL):
 
         # Exponential of radial basis functions
         K = np.exp(rbf)
-
         # Multiply by the vertical scale to get covariance matrix, K
-        self.K = np.multiply (pow(nu, 2), K)
+        self.K = np.multiply (pow(sigma, 2), K)
+        
 
         # Create C matrix for sampling perturbations
 
-        # add epsilon before scaling by a vertical variance scale, nu
+        # add epsilon before scaling by a vertical variance scale, sigma
         I = np.identity(self.nnodes)
-        K_tild = K = np.multiply(epsilon, I)
-        K_tild = np.multiply(pow(nu, 2), K_tild)
+        K_tild = np.add(K, np.multiply(epsilon, I))
+        K_tild = np.multiply(pow(sigma, 2), K_tild)
 
         self.C = np.linalg.cholesky(K_tild)
 
         #K = np.identity(self.nnodes)
-        #self.K = np.multiply(pow(nu, 2), K)
+        #self.K = np.multiply(pow(sigma, 2), K)
         #self.C = self.K
         #self.L_0 = np.sqrt(norms_matrix)
 